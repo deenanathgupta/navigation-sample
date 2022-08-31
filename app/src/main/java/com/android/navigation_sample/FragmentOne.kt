311cb694f12b5e72d71a0aff0d3c895012a689fe
deenanath.gupta <deenanath.gupta@cardinalhealth.com>
2022-08-31 22:55:06 +0530

    handled fragment transition

--- app/src/main/java/com/android/navigation_sample/FragmentOne.kt
+++ app/src/main/java/com/android/navigation_sample/FragmentOne.kt
@@ -1,11 +1,12 @@
 package com.android.navigation_sample
 
 import android.os.Bundle
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.android.navigation_sample.databinding.FragmentOneBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -14,12 +15,22 @@ import androidx.fragment.app.Fragment
  */
 class FragmentOne : Fragment() {
 
+    lateinit var binding: FragmentOneBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false)
+        binding = FragmentOneBinding.inflate(LayoutInflater.from(requireContext()))
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.txtOne.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_fragmentOne_to_fragmentTwo)
+        }
     }
 
     companion object {
--- app/src/main/java/com/android/navigation_sample/FragmentTwo.kt
+++ app/src/main/java/com/android/navigation_sample/FragmentTwo.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.android.navigation_sample.databinding.FragmentTwoBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -13,16 +15,21 @@ import androidx.fragment.app.Fragment
  */
 class FragmentTwo : Fragment() {
 
+    lateinit var binding: FragmentTwoBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false)
+        binding = FragmentTwoBinding.inflate(LayoutInflater.from(requireContext()))
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.txtTwo.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_fragmentTwo_to_fragmentOne)
+        }
     }
 
     companion object {
