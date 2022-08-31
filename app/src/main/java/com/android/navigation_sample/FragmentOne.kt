996c4b622bf652cf9cf7b72d4d17032a7f0aaddf
deenanath.gupta <deenanath.gupta@cardinalhealth.com>
2022-08-31 22:31:47 +0530

    fragment added

---
+++ app/src/main/java/com/android/navigation_sample/FragmentOne.kt
@@ -0,0 +1,36 @@
+package com.android.navigation_sample
+
+import android.os.Bundle
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentOne.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentOne : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment FragmentOne.
+         */
+        @JvmStatic
+        fun newInstance() =
+            FragmentOne()
+    }
+}
---
+++ app/src/main/java/com/android/navigation_sample/FragmentTwo.kt
@@ -0,0 +1,39 @@
+package com.android.navigation_sample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentTwo.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentTwo : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment FragmentTwo.
+         */
+        @JvmStatic
+        fun newInstance() =
+            FragmentTwo()
+    }
+}
